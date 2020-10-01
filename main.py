af4fdd6afeb4cfc63479cd7ad80b241d2cd31ffc
Azimov Parviz <lewdsaver@gmail.com>
2020-10-01 20:17:31 +0300

    initial

---
+++ main.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+import ssl
+import subprocess
+import os
+import pathlib
+
+def image_scrape(url, file_type, folder):
+        extension = file_type
+        i = 1
+        filename = folder + extension
+        r = requests.get(url)
+        with open(filename, 'wb') as outfile:
+            outfile.write(r.content)
+
+def create_directory(folder):
+    try:
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+    except OSError:
+        print("error creating a folder")
+        #error message is as horrible as i could think of so there is motivation to not make mistakes
+        raise
+
+
+ua = UserAgent(verify_ssl=False)
+create_directory("content");
+url = input("Input url to the album")
+response = requests.get(url, headers={'User-Agent':ua.chrome})
+soup = BeautifulSoup(response.text, 'html.parser')
+links = soup.find_all("a", class_="image")
+i = 0
+for link in links:
+    print(link)
+    try:
+        if '.png' in link['href']:
+            extension = '.png'
+        elif '.jpg' in link['href'] or '.jpeg' in link['href']:
+            extension = '.jpeg'
+        elif '.gif' in link['href']:
+            extension = '.gif'
+        else:
+            extension = ".jpg"
+        i += 1
+        folder = "content/" + str(i)
+        image_scrape(link['href'], extension, folder)
+    except ValueError:
+            pass
+
